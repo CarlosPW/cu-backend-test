442cf4e969cd234d7454b2e52d284147eb6c4b7d
Carlos Peñaloza <carlos818k@gmail.com>
2025-12-10 10:29:36 -0300

    cors

--- server.js
+++ server.js
@@ -3,9 +3,12 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
+
 
 // -----------------------------------
 // CONFIGURACIÓN
