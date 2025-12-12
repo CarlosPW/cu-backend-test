a643db2071df27e3f8baf27d9cda120ce7e248b6
Carlos Peñaloza <carlos818k@gmail.com>
2025-12-11 23:04:10 -0300

    fix get

--- controllers/sessionsController.js
+++ controllers/sessionsController.js
@@ -23,10 +23,13 @@ export const createSession = async (req, res) => {
 };
 
 export const getSession = async (req, res) => {
+  console.log('getSession', req.params);
   try {
-    const session = await db.select().from(sessions).where(eq(sessions.state, req.params.state));
-    res.status(200).json(session);
+    const session = await db.select().from(sessions).where(eq(sessions.state, req.params.id)).limit(1);
+    console.log('session', session);
+    res.status(200).json(session[0]);
   } catch (error) {
+    console.error('Full error:', error);
     res.status(500).json({ error: error.message });
   }
 };
